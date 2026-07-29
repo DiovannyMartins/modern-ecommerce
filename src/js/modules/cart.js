70815689a38e39008ac286a0e08a98e3c8d89892
Diovanny <betodiovanny@gmail.com>
2026-07-28 22:16:58 -0400

    refactor: remove Vite/build tools - pure HTML, CSS and JS only

--- src/js/modules/cart.js
+++ src/js/modules/cart.js
@@ -15,7 +15,7 @@ const btnCalcularFrete = document.getElementById("btnCalcularFrete");
 const freteResultado = document.getElementById("freteResultado");
 
 const nomeProdutoAtual = "Headset Wireless NeonX Pro";
-const imagemProdutoAtual = "img/frontal.webp";
+const imagemProdutoAtual = "src/img/frontal.webp";
 
 let carrinho = carregarStorage("carrinho", []);
 
