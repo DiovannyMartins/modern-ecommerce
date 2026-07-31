c9f24a6923f68e86f0303d6148d969584d68f8cf
Diovanny <betodiovanny@gmail.com>
2026-07-30 23:33:06 -0400

    fix: recria sw.js vazio com path relativo para substituir SW antigo e limpar cache

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", function() {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(key) { return caches.delete(key); }));
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(fetch(event.request));
+});
